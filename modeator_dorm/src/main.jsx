109b3d869ebbc5bc29b56d7fcadb563aba2ed869
EmmyLarsson <krittarphas2547@gmail.com>
2026-08-01 20:53:42 +0700

    ุึ67

--- modeator_dorm/src/main.jsx
+++ modeator_dorm/src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 // import './index.css'
-import './moderator.css'
-import Moderator from "./Moderator";
+import './modeator.css'
+import Modeartor from './modeartor.jsx'
 // import App from './App.jsx'
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      {/* <App /> */}
+     {/* <App /> */}
       <Moderator />
     </BrowserRouter>
   </StrictMode>
-);
+)
